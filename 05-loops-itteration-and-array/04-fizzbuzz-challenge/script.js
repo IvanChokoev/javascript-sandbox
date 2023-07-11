d7b771ac0e619e1145c76c3299d50a71ee937946
Ivan Chokoev <ivanchokoev96@gmail.com>
2023-07-11 15:20:35 +0300

    first commit

---
+++ 05-loops-itteration-and-array/04-fizzbuzz-challenge/script.js
@@ -0,0 +1,45 @@
+/* FizzBuzz Challenge
+
+Instructions:
+
+- Print/log the numbers from 1 to 100
+- For **multiples of three** print "Fizz" instead of the number
+- For **multiples of five** print "Buzz"
+- For numbers which are **multiples of both three and five** print "FizzBuzz".
+
+Hints:
+If you have taken the course up to this point, then you know how to write a loop and output/log something for each iteration. 
+You also know how to check for a condition with "if/else/else if". 
+You also know how to get a remainder of a number using the modulus operator (%). 
+This is all you need to know to complete this challenge. Good luck!
+ */
+
+// For Loop
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 15 === 0) {
+//     console.log('FizzBuzz');
+//   } else if (i % 3 === 0) {
+//     console.log('Fizz');
+//   } else if (i % 5 === 0) {
+//     console.log('Buzz');
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+// While Loop
+let j = 1;
+
+while (j <= 100) {
+  if (j % 15 === 0) {
+    console.log('FizzBuzz');
+  } else if (j % 3 === 0) {
+    console.log('Fizz');
+  } else if (j % 5 === 0) {
+    console.log('Buzz');
+  } else {
+    console.log(j);
+  }
+
+  j++;
+}
