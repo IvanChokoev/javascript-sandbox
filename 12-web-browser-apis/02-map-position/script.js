5b276587607a41a53ee089d13463bf9456a7934b
Ivan Chokoev <ivanchokoev96@gmail.com>
2023-08-08 17:03:07 +0300

    first commit

--- 12-web-browser-apis/02-map-position/script.js
+++ 12-web-browser-apis/02-map-position/script.js
@@ -1,8 +1,8 @@
-const map = L.map('map').setView([0, 0], 2);
+const map = L.map('map').setView([0,0], 2);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  attribution: 
+  '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 const marker = L.marker([0, 0]).addTo(map);
@@ -14,5 +14,5 @@ navigator.geolocation.getCurrentPosition(function (pos) {
   marker.setLatLng([lat, lng]).update();
   map.setView([lat, lng], 13);
 
-  marker.bindPopup('<strong>Hello World</strong> <br> This is my location');
-});
+  marker.bindPopup('<strong>Hi there!</strong> <br> This is my location!')
+})
